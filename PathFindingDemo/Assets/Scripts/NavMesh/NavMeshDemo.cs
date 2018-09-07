4401853251903fbee7515c5cc620597bb5ac16ee
linn <linn.guo@qq.com>
2018-09-07 10:10:14 +0800

    NavMesh tbd

---
+++ PathFindingDemo/Assets/Scripts/NavMesh/NavMeshDemo.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public partial class NavMeshDemo : MonoBehaviour
+{
+	public Material material;
+	NavMeshTriangulation? navMeshTriangulation = null;
+	NavMeshModel navMeshModel = null;
+
+	private void OnGUI()
+	{
+		if (GUILayout.Button("Export"))
+		{
+			ExportNavMesh();
+		}
+	}
+
+	public void ExportNavMesh()
+	{
+		navMeshTriangulation = NavMesh.CalculateTriangulation();
+		navMeshModel = new NavMeshModel(navMeshTriangulation.Value);
+	}
+
+	private void OnPostRender()
+	{
+		if (Camera.main != null && navMeshModel != null)
+		{
+			if (material != null)
+			{
+				material.SetPass(0);
+			}
+
+			GL.modelview = Camera.main.worldToCameraMatrix;
+			GL.LoadProjectionMatrix(Camera.main.projectionMatrix);
+
+			GL.Begin(GL.LINES);
+			GL.Color(Color.red);
+			
+			foreach (var node in navMeshModel.nodes)
+			{
+				foreach (var nei in node.neightbors)
+				{
+					if (nei != null)
+					{
+						GL.Vertex(node.center);
+						GL.Vertex(nei.center);
+					}
+				}
+
+				GL.Vertex(node.vertices[0]);
+				GL.Vertex(node.vertices[1]);
+
+				GL.Vertex(node.vertices[1]);
+				GL.Vertex(node.vertices[2]);
+
+				GL.Vertex(node.vertices[0]);
+				GL.Vertex(node.vertices[2]);
+			}
+			
+			GL.End();
+		}
+		
+	}
+}
---
+++ PathFindingDemo/Assets/Scripts/NavMesh/NavMeshModel.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshModel
+{
+	const int VertexPerNode = 3;
+
+	// NavMesh给的原始资源
+	Vector3[] navMeshVertices;
+	int[] navMeshIndices;
+
+	List<Vector3> vertices;
+	// 显示用的模型
+	Mesh mesh;
+
+	public class Node
+	{
+		public int[] indices = new int[3];
+		public Node[] neightbors = new Node[3];
+
+		public Vector3 center;
+		public Vector3[] vertices = new Vector3[3];
+	}
+	
+	// 节点
+	public List<Node> nodes;
+
+	private int FindFirst(List<Vector3> list, Vector3 value)
+	{
+		for (int i = 0; i < list.Count; ++i)
+		{
+			if (Vector3.Distance(list[i], value) < 0.1f)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private void Replace(int[] list, int oldValue, int newValue)
+	{
+		for (int i = 0; i < list.Length; ++i)
+		{
+			if (list[i] == oldValue)
+			{
+				list[i] = newValue;
+			}
+		}
+	}
+
+	public NavMeshModel(NavMeshTriangulation navMeshTriangulation)
+	{
+		navMeshVertices = navMeshTriangulation.vertices;
+		navMeshIndices = navMeshTriangulation.indices;
+
+		vertices = new List<Vector3>();
+
+		for (int i = 0; i < navMeshVertices.Length; ++i)
+		{
+			Vector3 curVex = navMeshVertices[i];
+			int idx = FindFirst(vertices, curVex);
+			if (idx >= 0)
+			{
+				Replace(navMeshIndices, i, idx);
+			}
+			else
+			{
+				vertices.Add(curVex);
+				Replace(navMeshIndices, i, vertices.Count - 1);
+			}
+		}
+
+		InitNodes();
+		InitNeighbors();
+	}
+
+	private void InitNodes()
+	{
+		nodes = new List<Node>();
+		
+		int nodesCount = navMeshIndices.Length / VertexPerNode;
+
+		for (int nodeIndex = 0; nodeIndex < nodesCount; ++nodeIndex)
+		{
+			Node node = new Node();
+			Vector3 center = Vector3.zero;
+			for (int i = 0; i < VertexPerNode; ++i)
+			{
+				int index = navMeshIndices[nodeIndex * VertexPerNode + i];
+				Vector3 vertex = vertices[index];
+				node.indices[i] = index;
+				node.vertices[i] = vertex;
+				center += vertex;
+			}
+			center /= VertexPerNode;
+			node.center = center;
+
+			nodes.Add(node);
+		}
+	}
+
+	private void InitNeighbors()
+	{
+		for (int i = 0; i < nodes.Count - 1; ++i)
+		{
+			for (int j = 0; j < nodes.Count; ++j)
+			{
+				CheckNeighbor(nodes[i], nodes[j]);
+				CheckNeighbor(nodes[j], nodes[i]);
+			}
+		}
+	}
+
+	private int FindFirst(int[] array, int value)
+	{
+		for (int i = 0; i < array.Length; ++i)
+		{
+			if (array[i] == value)
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	int[] aInb = new int[VertexPerNode];
+	int[] bIna = new int[VertexPerNode];
+	private void CheckNeighbor(Node a, Node b)
+	{
+		for (int i = 0; i < VertexPerNode; ++i)
+		{
+			int idx = FindFirst(b.indices, a.indices[i]);
+			aInb[i] = idx;
+		}
+
+		for (int i = 0; i < VertexPerNode; ++i)
+		{
+			int nextI = (i + 1) % VertexPerNode;
+			if (aInb[i] >= 0 && aInb[nextI] >= 0)
+			{
+				a.neightbors[i] = b;
+			}
+		}
+	}
+}
